7172b646c186e3bbb9058d9519216a4b71491654
Daniel Borner <71730916+DBorner@users.noreply.github.com>
2024-01-11 14:10:40 +0100

    Prepared basic views and styling

---
+++ app/navbar.tsx
@@ -0,0 +1,91 @@
+'use client';
+
+import { usePathname } from 'next/navigation';
+import { Disclosure, Menu, Transition } from '@headlessui/react';
+
+const navigation = [
+  { name: 'Dashboard', href: '/' },
+  { name: 'Playground', href: '/playground' }
+];
+
+function classNames(...classes: string[]) {
+  return classes.filter(Boolean).join(' ');
+}
+
+export default function Navbar() {
+  const pathname = usePathname();
+
+  return (
+    <Disclosure as="nav" className="bg-white shadow-sm">
+      {({ open }) => (
+        <>
+          <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+            <div className="flex h-16 justify-between">
+              <div className="flex">
+                <div className="flex flex-shrink-0 items-center">
+                  <svg
+                    width="32"
+                    height="32"
+                    viewBox="0 0 32 32"
+                    fill="none"
+                    className="text-gray-100"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <rect
+                      width="100%"
+                      height="100%"
+                      rx="16"
+                      fill="currentColor"
+                    />
+                    <image
+                    href="/logo.png"
+                    width="32"
+                    height="32"/>
+                  </svg>
+                </div>
+                <div className="hidden sm:-my-px sm:ml-6 sm:flex sm:space-x-8">
+                  {navigation.map((item) => (
+                    <a
+                      key={item.name}
+                      href={item.href}
+                      className={classNames(
+                        pathname === item.href
+                          ? 'border-slate-500 text-gray-900'
+                          : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300',
+                        'inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium'
+                      )}
+                      aria-current={pathname === item.href ? 'page' : undefined}
+                    >
+                      {item.name}
+                    </a>
+                  ))}
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <Disclosure.Panel className="sm:hidden">
+            <div className="space-y-1 pt-2 pb-3">
+              {navigation.map((item) => (
+                <Disclosure.Button
+                  key={item.name}
+                  as="a"
+                  href={item.href}
+                  className={classNames(
+                    pathname === item.href
+                      ? 'bg-slate-50 border-slate-500 text-slate-700'
+                      : 'border-transparent text-gray-600 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-800',
+                    'block pl-3 pr-4 py-2 border-l-4 text-base font-medium'
+                  )}
+                  aria-current={pathname === item.href ? 'page' : undefined}
+                >
+                  {item.name}
+                </Disclosure.Button>
+              ))}
+            </div>
+          </Disclosure.Panel>
+        </>
+      )}
+    </Disclosure>
+  );
+}
--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'avatars.githubusercontent.com'
+        },
+        {
+          protocol: 'https',
+          hostname: 'avatar.vercel.sh'
+        }
+      ]
+    }
+  };
+  
+  module.exports = nextConfig;
+  
