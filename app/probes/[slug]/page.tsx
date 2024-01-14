759a2eca409ab6902c08c0277041436a5a045dcd
Daniel Borner <71730916+DBorner@users.noreply.github.com>
2024-01-14 16:58:52 +0100

    Finished probe detail view

--- app/probes/[slug]/page.tsx
+++ app/probes/[slug]/page.tsx
@@ -1,9 +1,14 @@
 'use client';
 
-import { Card, Title, Text, Grid, Col } from '@tremor/react';
+import { Card, Title, Text, Grid, Col, LineChart, DateRangePicker,  Metric, Flex, CategoryBar, Divider, AreaChart, Icon, Badge, DateRangePickerValue } from '@tremor/react';
 import ReadingsTable from './table';
 import {  useState, useEffect } from 'react';
 import toast from 'react-hot-toast';
+import { Radio } from 'react-loader-spinner';
+import { SignalIcon, SignalSlashIcon } from '@heroicons/react/24/solid';
+import { it } from 'node:test';
+
+
 
 type Reading = {
   id: string;
@@ -32,6 +37,9 @@ type ProbeData = {
   soil_moisture_ranges: number[];
 };
 
+const temperatureValueFormatter = (number: number | bigint) => `${new Intl.NumberFormat("us").format(number).toString()}°C`;
+const percentValueFormatter = (number: number | bigint) => `${new Intl.NumberFormat("us").format(number).toString()} %`;
+  
 
 export default function IndexPage({ params }: { params: { slug: string } }) {
 
@@ -39,6 +47,7 @@ export default function IndexPage({ params }: { params: { slug: string } }) {
   const [readings, setReadings] = useState<Reading[]>([]);
   const [last_read_time, setLastReadTime] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [fullData, setFullData] = useState<ProbeData>();
 
   useEffect(() => {
     async function fetchData() {
@@ -46,6 +55,23 @@ export default function IndexPage({ params }: { params: { slug: string } }) {
     }
     fetchData();
   }, []);
+
+  const handleDateRangeChange: (value: DateRangePickerValue) => void = (value) => {
+    const from = value.from;
+    const to = value.to;
+    if (!from || !to){
+      setData(fullData);
+      return;
+    };
+    const filtered_data = fullData?.data.filter((item) => {
+      const item_time = new Date(item.time);
+      item_time.setHours(0);
+      item_time.setMinutes(0);
+      item_time.setSeconds(0);
+      return (item_time >= from && item_time <= to);
+    });
+    setData({...data, data: filtered_data});
+  };
 
   const fetchNewData = async () => {
     const response = await fetch('http://127.0.0.1:8000/api/probes/details/'+params.slug+"/").catch((error) => {
@@ -55,6 +81,7 @@ export default function IndexPage({ params }: { params: { slug: string } }) {
     const data = await response.json();
     console.log(data);
     setData(data.data);
+    setFullData(data.data);
     setReadings(data.data.data);
     setLastReadTime(data.read_time);
     setIsLoading(false);
@@ -70,36 +97,147 @@ export default function IndexPage({ params }: { params: { slug: string } }) {
 
   return (
     <main className="p-4 md:p-10 mx-auto max-w-7xl">
-      <Title>Probe 1</Title>
-      <Text>Lorem ipsum dolor sit amet, consetetur sadipscing elitr.</Text>
+      {isLoading ? <div className="flex justify-center items-center">
+          <>
+          <Title className="mr-4">Loading...</Title>
+          <Radio
+              visible={true}
+              height="30"
+              width="30"
+              colors={["#6366F1", "#6366F1", "#6366F1"]}
+              ariaLabel="radio-loading"/>
+            </>
+          </div> : 
+      <>
+      <Flex>
+      <Title >{data?.name}</Title>
+      <Badge icon={data?.is_active ? SignalIcon : SignalSlashIcon} color={data?.is_active ? "emerald" : "red"}>{data?.is_active ? "Active" : "Inactive"}</Badge>
+      </Flex>
+      <Text>{data?.plant} - {data?.plant_species}</Text><Grid numItemsLg={6} className="gap-6 mt-6">
+          <Col numColSpanLg={4}>
+            <Card className="h-full overflow-hidden">
+              <div className="h-60">
+                <Text>Readings table</Text>
+                <ReadingsTable data={readings} api_url={'http://127.0.0.1:8000/api/probes/details/' + params.slug + "/"} />
+              </div>
+            </Card>
+          </Col>
 
-      <Grid numItemsLg={6} className="gap-6 mt-6">
-        <Col numColSpanLg={4}>
-          <Card className="h-full">
-            <div className="h-60">
-              <Text>Readings table</Text>
-              <ReadingsTable readings={readings} />
+          <Col numColSpanLg={2}>
+            <div className="space-y-6">
+            <Card>
+                <div className="h-30">
+                  
+                  <Text>Last update time:</Text>
+                  <Metric>{data?.last_read_time}</Metric>
+                </div>
+              </Card>
+              <Card>
+                <div className="h-58">
+                  <Flex>
+                    <Text>Temperature</Text>
+                    <Text>{data?.temperature.toFixed(2)} °C</Text>
+                  </Flex>
+                  <CategoryBar
+                    values={data?.temperature_ranges}
+                    colors={["rose", "orange", "yellow", "emerald", "yellow", "orange", "rose"]}
+                    markerValue={data?.temperature}
+                    showLabels={false}
+                    className="mt-1" />
+                  <br />
+                  <Flex>
+                    <Text>Light level</Text>
+                    <Text>{data?.sunlight_procent.toFixed(2)} %</Text>
+                  </Flex>
+                  <CategoryBar
+                    values={data?.sunlight_ranges}
+                    colors={["rose", "orange", "yellow", "emerald", "yellow", "orange", "rose"]}
+                    markerValue={data?.sunlight_procent}
+                    showLabels={false}
+                    className="mt-1" />
+                  <br />
+                  <Flex>
+                    <Text>Humidity</Text>
+                    <Text>{data?.humidity.toFixed(2)} %</Text>
+                  </Flex>
+                  <CategoryBar
+                    values={data?.humidity_ranges}
+                    colors={["rose", "orange", "yellow", "emerald", "yellow", "orange", "rose"]}
+                    markerValue={data?.humidity}
+                    showLabels={false}
+                    className="mt-1" />
+                  <br />
+
+                  <Flex>
+                    <Text>Mouisture of soil</Text>
+                    <Text>{data?.soil_moisture.toFixed(2)} %</Text>
+                  </Flex>
+                  <CategoryBar
+                    values={data?.soil_moisture_ranges}
+                    colors={["rose", "orange", "yellow", "emerald", "yellow", "orange", "rose"]}
+                    markerValue={data?.soil_moisture}
+                    showLabels={false}
+                    className="mt-1" />
+                </div>
+              </Card>
             </div>
-          </Card>
-        </Col>
+          </Col>
+        </Grid>
+        <Card className="mt-6">
+          <Flex>
+            <Text>Temperature chart</Text>
+            <DateRangePicker
+              className="ml-4"
+              onValueChange={handleDateRangeChange}
+              />
+          </Flex>
+          <AreaChart
+                  data={data?.data}
+                  index="time"
+                  categories={["Temperature"]}
+                  colors={["emerald", "gray"]}
+                  valueFormatter={temperatureValueFormatter}
+                  showXAxis={false}
+                  
+                  yAxisWidth={50}
+                />
+          <Divider/>
+          <Text>Humidity chart</Text>
+          <AreaChart
+                data={data?.data}
+                index="time"
+                categories={["Humidity"]}
+                colors={["sky"]}
+                valueFormatter={percentValueFormatter}
+                showXAxis={false}
+                yAxisWidth={50}
+              ></AreaChart>
+          <Divider/>
 
-        <Col numColSpanLg={2}>
-          <div className="space-y-6">
-            <Card>
-              <div className="h-24" />
-            </Card>
-            <Card>
-              <div className="h-24" />
-            </Card>
-            <Card>
-              <div className="h-24" />
-            </Card>
-            <Card>
-              <div className="h-24" />
-            </Card>
-          </div>
-        </Col>
-      </Grid>
+          <Text>Light level chart</Text>
+          <AreaChart
+                data={data?.data}
+                index="time"
+                categories={["Light level"]}
+                colors={["yellow"]}
+                valueFormatter={percentValueFormatter}
+                showXAxis={false}
+                yAxisWidth={50}
+              ></AreaChart>
+          <Divider/>
+          <Text>Mouisture of soil chart</Text>
+          <AreaChart
+                data={data?.data}
+                index="time"
+                categories={["Mouisture of soil"]}
+                colors={["lime"]}
+                valueFormatter={percentValueFormatter}
+                showXAxis={false}
+                yAxisWidth={50}
+              ></AreaChart>
+        </Card>
+        </>
+}
     </main>
   );
 }
--- app/probes/[slug]/table.tsx
+++ app/probes/[slug]/table.tsx
@@ -1,44 +1,121 @@
-import {
-    Table,
-    TableRow,
-    TableCell,
-    TableHead,
-    TableHeaderCell,
-    TableBody,
-  } from "@tremor/react";
-  
-    type Reading = {
-        id: string;
-        time: Date;
-        "Temperature": number;
-        "Humidity": number;
-        "Light level": number;
-        "Mouisture of soil": number;
-    };
-  
-  export default function ReadingsTable({ readings }: { readings: Reading[] }) {
-    return (
-    <Table className="mt-6">
-        <TableHead>
-        <TableRow>
-            <TableHeaderCell>Date and Time</TableHeaderCell>
-            <TableHeaderCell>Temperature</TableHeaderCell>
-            <TableHeaderCell>Humidity</TableHeaderCell>
-            <TableHeaderCell>Sunlight Level</TableHeaderCell>
-            <TableHeaderCell>Soil Moisture</TableHeaderCell>
-        </TableRow>
-        </TableHead>
-        <TableBody>
-        {readings.map((item) => (
-            <TableRow key={item.id}>
-            <TableCell>{item.time.toLocaleString()}</TableCell>
-            <TableCell>{item["Temperature"].toFixed(2)} °C</TableCell>
-            <TableCell>{item["Humidity"].toFixed(2)} %</TableCell>
-            <TableCell>{item["Light level"].toFixed(2)} %</TableCell>
-            <TableCell>{item["Mouisture of soil"].toFixed(2)} %</TableCell>
-            </TableRow>
-        ))}
-        </TableBody>
-    </Table>
+import React from "react";
+import {Table, TableHeader, TableColumn, TableBody, TableRow, TableCell, Pagination, Spinner, getKeyValue} from "@nextui-org/react";
+import {useAsyncList} from "@react-stately/data";
+
+type Reading = {
+    id: string;
+    time: string;
+    "Temperature": number;
+    "Humidity": number;
+    "Light level": number;
+    "Mouisture of soil": number;
+  };
+
+export default function ReadingsTable({data, api_url}: {data: Reading[], api_url: string}) {
+    const [page, setPage] = React.useState(1);
+    const rowsPerPage = 6;
+
+    const [isLoading, setIsLoading] = React.useState(true);
+
+    const converded_data = data.map((item) => {
+        return {
+            time: item.time,
+            temperature: item.Temperature.toFixed(2),
+            humidity: item.Humidity.toFixed(2),
+            light_level: item["Light level"].toFixed(2),
+            soil_mouisture: item["Mouisture of soil"].toFixed(2),
+        }
+        }
     );
-  }
+
+    const pages = Math.ceil(data.length / rowsPerPage);
+
+    let list = useAsyncList({
+        async load({signal}) {
+            let res = await fetch(api_url);
+            let json = await res.json();
+            setIsLoading(false);
+            return {
+                items: json.data.data,
+            };
+        },
+        async sort({items, sortDescriptor}) {
+            if (sortDescriptor.column === "time"){
+                return {
+                    items: items.sort((a, b) => {
+                        let first = new Date(a[sortDescriptor.column]);
+                        let second = new Date(b[sortDescriptor.column]);
+                        let cmp = first < second ? -1 : 1;
+              
+                        if (sortDescriptor.direction === "descending") {
+                          cmp *= -1;
+                        }
+              
+                        return cmp;
+                      }),
+                }
+            }
+          return {
+            items: items.sort((a, b) => {
+              let first = a[sortDescriptor.column];
+              let second = b[sortDescriptor.column];
+              let cmp = (parseInt(first) || first) < (parseInt(second) || second) ? -1 : 1;
+    
+              if (sortDescriptor.direction === "descending") {
+                cmp *= -1;
+              }
+    
+              return cmp;
+            }),
+          };
+        },
+      });
+
+      const items = React.useMemo(() => {
+        const start = (page - 1) * rowsPerPage;
+        const end = start + rowsPerPage;
+    
+        return list.items.slice(start, end);
+      }, [page, list.items]);
+  
+  
+    return (
+      <Table 
+        removeWrapper
+        aria-label="Example table with client side pagination"
+        sortDescriptor={list.sortDescriptor}
+        onSortChange={list.sort}
+        bottomContent={
+          <div className="flex w-full justify-center">
+            <Pagination
+              isCompact
+              showControls
+              showShadow
+              color="secondary"
+              page={page}
+              total={Math.ceil(list.items.length / rowsPerPage)}
+              onChange={(page) => setPage(page)}
+            />
+          </div>
+        }
+        classNames={{
+          wrapper: "min-h-[222px]",
+        }}
+      >
+        <TableHeader>
+          <TableColumn key="time" allowsSorting>Time</TableColumn>
+          <TableColumn key="Temperature" allowsSorting>Temperature (°C)</TableColumn>
+          <TableColumn key="Humidity" allowsSorting>Humidity (%)</TableColumn>
+          <TableColumn key="Light level" allowsSorting>Light level (%)</TableColumn>
+          <TableColumn key="Mouisture of soil" allowsSorting>Mouisture of soil (%)</TableColumn>
+        </TableHeader>
+        <TableBody items={items} >
+          {(item) => (
+            <TableRow key={item.time}>
+              {(columnKey) => <TableCell>{columnKey!="time"? getKeyValue(item, columnKey).toFixed(2) : getKeyValue(item, columnKey)}</TableCell>}
+            </TableRow>
+          )}
+        </TableBody>
+      </Table>
+    );
+}
